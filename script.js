155bff968979a925b934cbafbac244ca2c4ee82d
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-19 23:27:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
 //your JS code here. If required.
- function onDOMLoad() {
-            document.body.innerHTML += "<p>DOM load success</p>";
-        }
-        document.addEventListener("DOMContentLoaded", onDOMLoad);
