4fec792068cddefe239e674d65161b1f73fd96e4
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-19 23:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 //your JS code here. If required.
-function onDOMLoad() {
-            if (document.body.children.length === 0) {
-                document.body.innerHTML = "DOM load success";
-            } else {
-                console.warn("Body already contains children. The string 'DOM load success' was not written.");
-            }
+ function onDOMLoad() {
+            document.body.innerHTML += "<p>DOM load success</p>";
         }
         document.addEventListener("DOMContentLoaded", onDOMLoad);
