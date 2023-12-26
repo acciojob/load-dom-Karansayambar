13d8b5b09c2f17eff2bf8f45931fd48916239054
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-26 15:00:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
 
-document.addEventListener("DOMContentLoaded", function() {
-    function onDOMLoad() {
-        const para = document.createElement("p");
-        para.innerText = "DOM load success";
-        document.body.appendChild(para);
-    }
-    onDOMLoad(); // Call the function after DOMContentLoaded
-});
+// document.addEventListener("DOMContentLoaded",function onDOMLoad() {
+//         const para = document.createElement("p");
+//         para.innerText = "DOM load success";
+//         document.body.appendChild(para);
+//     }
+//     onDOMLoad(); // Call the function after DOMContentLoaded
+// });
+
